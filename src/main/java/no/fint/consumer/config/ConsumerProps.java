5142795fbddb9d611f8f1dbf96b1e1789335249b
Asgeir Nilsen <asgeir@fintprosjektet.no>
2017-10-20 13:20:46 +0200

    Fixed error.

--- src/main/java/no/fint/consumer/config/ConsumerProps.java
+++ src/main/java/no/fint/consumer/config/ConsumerProps.java
@@ -9,15 +9,15 @@ import org.springframework.stereotype.Component;
 public class ConsumerProps {
 
 	public static final String CACHE_FIXEDRATE_FYLKE = "${fint.consumer.cache.fixedRate.fylke:900000}";
-	public static final String CACHE_FIXEDRATE_KJONN = "${fint.consumer.cache.fixedRate.kjonn:900000";
+	public static final String CACHE_FIXEDRATE_KJONN = "${fint.consumer.cache.fixedRate.kjonn:900000}";
 	public static final String CACHE_FIXEDRATE_KOMMUNE = "${fint.consumer.cache.fixedRate.kommune:900000}";
-	public static final String CACHE_FIXEDRATE_LANDKODE = "${fint.consumer.cache.fixedRate.landkode:900000";
-	public static final String CACHE_FIXEDRATE_SPRAK = "${fint.consumer.cache.fixedRate.sprak:900000";
+	public static final String CACHE_FIXEDRATE_LANDKODE = "${fint.consumer.cache.fixedRate.landkode:900000}";
+	public static final String CACHE_FIXEDRATE_SPRAK = "${fint.consumer.cache.fixedRate.sprak:900000}";
 	public static final String CACHE_INITIALDELAY_FYLKE = "${fint.consumer.cache.initialDelay.fylke:50000}";
-	public static final String CACHE_INITIALDELAY_KJONN = "${fint.consumer.cache.initialDelay.kjonn:60000";
+	public static final String CACHE_INITIALDELAY_KJONN = "${fint.consumer.cache.initialDelay.kjonn:60000}";
 	public static final String CACHE_INITIALDELAY_KOMMUNE = "${fint.consumer.cache.initialDelay.kommune:70000}";
-	public static final String CACHE_INITIALDELAY_LANDKODE = "${fint.consumer.cache.initialDelay.landkode:80000";
-	public static final String CACHE_INITIALDELAY_SPRAK = "${fint.consumer.cache.initialDelay.sprak:90000";
+	public static final String CACHE_INITIALDELAY_LANDKODE = "${fint.consumer.cache.initialDelay.landkode:80000}";
+	public static final String CACHE_INITIALDELAY_SPRAK = "${fint.consumer.cache.initialDelay.sprak:90000}";
 
 
 	@Value("${fint.events.orgIds:fint.no}")
