22eb0c84df59625c1ba7fe6a7978dae1eb576b8b
Asgeir Nilsen <asgeir@fintlabs.no>
2021-02-12 11:26:04 +0100

    Use StringUtils.substringAfter.

--- src/main/java/no/fint/consumer/models/iso/IsoController.java
+++ src/main/java/no/fint/consumer/models/iso/IsoController.java
@@ -1,18 +1,20 @@
 package no.fint.consumer.models.iso;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Slf4j
 public class IsoController {
 
     @RequestMapping("/iso/**")
-    public ModelAndView redirectIso() {
-        final String viewName = "forward:" + ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString().replace("/iso/", "/");
+    public ModelAndView redirectIso(HttpServletRequest request) {
+        final String viewName = "forward:" + StringUtils.substringAfter(request.getRequestURI(), "/iso");
         log.info(viewName);
         return new ModelAndView(viewName);
     }
