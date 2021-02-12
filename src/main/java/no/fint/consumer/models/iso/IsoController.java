9c238eb032675fc902bd20efef5b45b7ddb42383
Asgeir Nilsen <asgeir@fintlabs.no>
2021-02-12 11:21:48 +0100

    Use ServletUriComponentsBuilder.

--- src/main/java/no/fint/consumer/models/iso/IsoController.java
+++ src/main/java/no/fint/consumer/models/iso/IsoController.java
@@ -4,16 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @Slf4j
 public class IsoController {
 
     @RequestMapping("/iso/**")
-    public ModelAndView redirectIso(HttpServletRequest request) {
-        final String viewName = "forward:" + request.getRequestURI().replace("/iso", "");
+    public ModelAndView redirectIso() {
+        final String viewName = "forward:" + ServletUriComponentsBuilder.fromCurrentRequest().build().toUriString().replace("/iso/", "/");
         log.info(viewName);
         return new ModelAndView(viewName);
     }
