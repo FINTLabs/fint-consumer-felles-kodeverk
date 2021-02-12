4448d9988ef34f072727377d776eec45d39097ba
Asgeir Nilsen <asgeir@fintlabs.no>
2021-02-12 11:31:03 +0100

    Remove logging.

--- src/main/java/no/fint/consumer/models/iso/IsoController.java
+++ src/main/java/no/fint/consumer/models/iso/IsoController.java
@@ -1,6 +1,5 @@
 package no.fint.consumer.models.iso;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +8,10 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@Slf4j
 public class IsoController {
 
     @RequestMapping("/iso/**")
     public ModelAndView redirectIso(HttpServletRequest request) {
-        final String viewName = "forward:" + StringUtils.substringAfter(request.getRequestURI(), "/iso");
-        log.info(viewName);
-        return new ModelAndView(viewName);
+        return new ModelAndView("forward:" + StringUtils.substringAfter(request.getRequestURI(), "/iso"));
     }
 }
