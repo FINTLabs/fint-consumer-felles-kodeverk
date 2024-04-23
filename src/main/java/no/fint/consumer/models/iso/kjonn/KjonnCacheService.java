62f8d22ff3a65d59871916d8cd18e262c879df3a
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2024-04-23 19:42:04 +0200

    update cache (#24)

--- src/main/java/no/fint/consumer/models/iso/kjonn/KjonnCacheService.java
+++ src/main/java/no/fint/consumer/models/iso/kjonn/KjonnCacheService.java
@@ -107,7 +107,10 @@ public class KjonnCacheService extends CacheService<KjonnResource> {
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (IsoActions.valueOf(event.getAction()) == IsoActions.UPDATE_KJONN) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<KjonnResource>> cacheObjects = data
