1a5ddab7321c31fc51e39819748ce4b49db10d5d
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-01-15 15:28:57 +0100

    Version 4.0.0-rc-6.

---
+++ src/main/java/no/novari/fint/consumer/models/iso/kjonn/KjonnCacheService.java
@@ -0,0 +1,135 @@
+package no.novari.fint.consumer.models.iso.kjonn;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import lombok.extern.slf4j.Slf4j;
+
+import no.fint.cache.CacheService;
+import no.fint.cache.model.CacheObject;
+import no.novari.fint.consumer.config.Constants;
+import no.novari.fint.consumer.config.ConsumerProps;
+import no.novari.fint.consumer.event.ConsumerEventUtil;
+import no.fint.event.model.Event;
+import no.fint.event.model.ResponseStatus;
+import no.novari.fint.relations.FintResourceCompatibility;
+import no.novari.fint.model.resource.FintLinks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import no.novari.fint.model.felles.kodeverk.iso.Kjonn;
+import no.novari.fint.model.resource.felles.kodeverk.iso.KjonnResource;
+import no.novari.fint.model.felles.kodeverk.iso.IsoActions;
+import no.novari.fint.model.felles.kompleksedatatyper.Identifikator;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(name = "fint.consumer.cache.disabled.kjonn", havingValue = "false", matchIfMissing = true)
+public class KjonnCacheService extends CacheService<KjonnResource> {
+
+    public static final String MODEL = Kjonn.class.getSimpleName().toLowerCase();
+
+    @Value("${fint.consumer.compatibility.fintresource:true}")
+    private boolean checkFintResourceCompatibility;
+
+    @Autowired
+    private FintResourceCompatibility fintResourceCompatibility;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private KjonnLinker linker;
+
+    private JavaType javaType;
+
+    private ObjectMapper objectMapper;
+
+    public KjonnCacheService() {
+        super(MODEL, IsoActions.GET_ALL_KJONN, IsoActions.UPDATE_KJONN);
+        objectMapper = new ObjectMapper();
+        javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, KjonnResource.class);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    @PostConstruct
+    public void init() {
+        props.getAssets().forEach(this::createCache);
+    }
+
+    @Scheduled(initialDelayString = Constants.CACHE_INITIALDELAY_KJONN, fixedRateString = Constants.CACHE_FIXEDRATE_KJONN)
+    public void populateCacheAll() {
+        props.getAssets().forEach(this::populateCache);
+    }
+
+    public void rebuildCache(String orgId) {
+		flush(orgId);
+		populateCache(orgId);
+	}
+
+    @Override
+    public void populateCache(String orgId) {
+		log.info("Populating Kjonn cache for {}", orgId);
+        Event event = new Event(orgId, Constants.COMPONENT, IsoActions.GET_ALL_KJONN, Constants.CACHE_SERVICE);
+        consumerEventUtil.send(event);
+    }
+
+
+    public Optional<KjonnResource> getKjonnBySystemId(String orgId, String systemId) {
+        return getOne(orgId, systemId.hashCode(),
+            (resource) -> Optional
+                .ofNullable(resource)
+                .map(KjonnResource::getSystemId)
+                .map(Identifikator::getIdentifikatorverdi)
+                .map(systemId::equals)
+                .orElse(false));
+    }
+
+
+	@Override
+    public void onAction(Event event) {
+        List<KjonnResource> data;
+        if (checkFintResourceCompatibility && fintResourceCompatibility.isFintResourceData(event.getData())) {
+            log.info("Compatibility: Converting FintResource<KjonnResource> to KjonnResource ...");
+            data = fintResourceCompatibility.convertResourceData(event.getData(), KjonnResource.class);
+        } else {
+            data = objectMapper.convertValue(event.getData(), javaType);
+        }
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
+        if (IsoActions.valueOf(event.getAction()) == IsoActions.UPDATE_KJONN) {
+            if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
+                List<CacheObject<KjonnResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+                addCache(event.getOrgId(), cacheObjects);
+                log.info("Added {} cache objects to cache for {}", cacheObjects.size(), event.getOrgId());
+            } else {
+                log.debug("Ignoring payload for {} with response status {}", event.getOrgId(), event.getResponseStatus());
+            }
+        } else {
+            List<CacheObject<KjonnResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+            updateCache(event.getOrgId(), cacheObjects);
+            log.info("Updated cache for {} with {} cache objects", event.getOrgId(), cacheObjects.size());
+        }
+    }
+}
---
+++ src/main/java/no/novari/fint/consumer/models/iso/sprak/SprakCacheService.java
@@ -0,0 +1,135 @@
+package no.novari.fint.consumer.models.iso.sprak;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import lombok.extern.slf4j.Slf4j;
+
+import no.fint.cache.CacheService;
+import no.fint.cache.model.CacheObject;
+import no.novari.fint.consumer.config.Constants;
+import no.novari.fint.consumer.config.ConsumerProps;
+import no.novari.fint.consumer.event.ConsumerEventUtil;
+import no.fint.event.model.Event;
+import no.fint.event.model.ResponseStatus;
+import no.novari.fint.relations.FintResourceCompatibility;
+import no.novari.fint.model.resource.FintLinks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import no.novari.fint.model.felles.kodeverk.iso.Sprak;
+import no.novari.fint.model.resource.felles.kodeverk.iso.SprakResource;
+import no.novari.fint.model.felles.kodeverk.iso.IsoActions;
+import no.novari.fint.model.felles.kompleksedatatyper.Identifikator;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(name = "fint.consumer.cache.disabled.sprak", havingValue = "false", matchIfMissing = true)
+public class SprakCacheService extends CacheService<SprakResource> {
+
+    public static final String MODEL = Sprak.class.getSimpleName().toLowerCase();
+
+    @Value("${fint.consumer.compatibility.fintresource:true}")
+    private boolean checkFintResourceCompatibility;
+
+    @Autowired
+    private FintResourceCompatibility fintResourceCompatibility;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private SprakLinker linker;
+
+    private JavaType javaType;
+
+    private ObjectMapper objectMapper;
+
+    public SprakCacheService() {
+        super(MODEL, IsoActions.GET_ALL_SPRAK, IsoActions.UPDATE_SPRAK);
+        objectMapper = new ObjectMapper();
+        javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, SprakResource.class);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+    }
+
+    @PostConstruct
+    public void init() {
+        props.getAssets().forEach(this::createCache);
+    }
+
+    @Scheduled(initialDelayString = Constants.CACHE_INITIALDELAY_SPRAK, fixedRateString = Constants.CACHE_FIXEDRATE_SPRAK)
+    public void populateCacheAll() {
+        props.getAssets().forEach(this::populateCache);
+    }
+
+    public void rebuildCache(String orgId) {
+		flush(orgId);
+		populateCache(orgId);
+	}
+
+    @Override
+    public void populateCache(String orgId) {
+		log.info("Populating Sprak cache for {}", orgId);
+        Event event = new Event(orgId, Constants.COMPONENT, IsoActions.GET_ALL_SPRAK, Constants.CACHE_SERVICE);
+        consumerEventUtil.send(event);
+    }
+
+
+    public Optional<SprakResource> getSprakBySystemId(String orgId, String systemId) {
+        return getOne(orgId, systemId.hashCode(),
+            (resource) -> Optional
+                .ofNullable(resource)
+                .map(SprakResource::getSystemId)
+                .map(Identifikator::getIdentifikatorverdi)
+                .map(systemId::equals)
+                .orElse(false));
+    }
+
+
+	@Override
+    public void onAction(Event event) {
+        List<SprakResource> data;
+        if (checkFintResourceCompatibility && fintResourceCompatibility.isFintResourceData(event.getData())) {
+            log.info("Compatibility: Converting FintResource<SprakResource> to SprakResource ...");
+            data = fintResourceCompatibility.convertResourceData(event.getData(), SprakResource.class);
+        } else {
+            data = objectMapper.convertValue(event.getData(), javaType);
+        }
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
+        if (IsoActions.valueOf(event.getAction()) == IsoActions.UPDATE_SPRAK) {
+            if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
+                List<CacheObject<SprakResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+                addCache(event.getOrgId(), cacheObjects);
+                log.info("Added {} cache objects to cache for {}", cacheObjects.size(), event.getOrgId());
+            } else {
+                log.debug("Ignoring payload for {} with response status {}", event.getOrgId(), event.getResponseStatus());
+            }
+        } else {
+            List<CacheObject<SprakResource>> cacheObjects = data
+                    .stream()
+                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                    .collect(Collectors.toList());
+            updateCache(event.getOrgId(), cacheObjects);
+            log.info("Updated cache for {} with {} cache objects", event.getOrgId(), cacheObjects.size());
+        }
+    }
+}
